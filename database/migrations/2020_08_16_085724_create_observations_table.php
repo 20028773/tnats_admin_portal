34e4db62371b1a811adf4dd95cf0c5c2f09b5933
Koji Furukawa <20028773@tafe.wa.edu.au>
2020-10-25 21:48:38 +0800

    database changes

--- database/migrations/2020_08_16_085724_create_observations_table.php
+++ database/migrations/2020_08_16_085724_create_observations_table.php
@@ -19,8 +19,7 @@ class CreateObservationsTable extends Migration
             $table->integer('user_id');
             $table->string('location')->nullable();
             $table->string('species');
-            $table->string('notes');
-            //$table->longText('photo');
+            $table->string('notes'); 
             $table->boolean('approved');
             $table->boolean('active');
             $table->double('longitude')->nullable();
