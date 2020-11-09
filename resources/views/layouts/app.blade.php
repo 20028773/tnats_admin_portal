d6b8aeff9d0129549e10637c86cd784eea1d8fd1
Koji Furukawa <20028773@tafe.wa.edu.au>
2020-11-09 10:06:04 +0800

    navigtion fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,11 +57,9 @@
 
                                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                         <a class="dropdown-item" href='{{url("/users")}}'>Users</a>
-                                    </div>
-
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                         <a class="dropdown-item" href='{{url("/observations")}}'>Observations</a>
                                     </div>
+
                                 </li>
                             @endif
 
